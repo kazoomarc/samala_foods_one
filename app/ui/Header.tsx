fe3e74be815a49bf5a6cd2465224982ceb908499
Joel Mwala <joelmwala7@gmail.com>
2024-06-30 00:27:03 +0200

    update header colors

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div className="py-4 text-base font-medium bg-orange-500">
+    <div className="py-4 text-base font-medium bg-black _bg-orange-700">
       <div className="px-28">
         <nav className="flex justify-between">
           <div>
-            <span className="font-playfair">
+            <span className="font-playfair tracking-wider text-lg text-white font-bold">
               <Link href={'/'}>SAMALA</Link>
             </span>
           </div>
-          <div className="flex gap-8">
+          <div className="flex gap-8 text-white tracking-wide">
             <Link href="/menu">Menu</Link>
             <Link href="/about">About</Link>
             <Link href="/contact">Contact</Link>
