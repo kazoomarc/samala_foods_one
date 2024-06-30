2f757201119757a10f934c1944b1529624f0ca70
Joel Mwala <joelmwala7@gmail.com>
2024-06-30 12:29:50 +0200

    make header rsponsive

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <div className="py-4 text-base font-medium bg-black _bg-orange-700">
-      <div className="px-28">
+      <div className="px-4 sm:px-6 md:px-10 lg:px-16 xl:px-20 2xl:px-28">
         <nav className="flex justify-between">
           <div>
             <span className="font-playfair tracking-wider text-lg text-white font-bold">
               <Link href={'/'}>SAMALA</Link>
             </span>
           </div>
-          <div className="flex gap-8 text-white tracking-wide">
+          <div className="hidden sm:flex gap-8 text-white tracking-wide">
             <Link href="/menu">Menu</Link>
             <Link href="/about">About</Link>
             <Link href="/contact">Contact</Link>
